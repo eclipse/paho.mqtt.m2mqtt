0d922519f1ff2b5a3b36d78a7b5be1064ad2f526
Robin Jones <networkfusion@users.noreply.github.com>
2019-07-15 09:44:18 +0100

    Improve handling of SslProtocols (#3)
    ***NO_CI***

--- M2Mqtt/Net/MqttNetworkChannel.cs
+++ M2Mqtt/Net/MqttNetworkChannel.cs
@@ -467,15 +467,17 @@ namespace uPLibrary.Networking.M2Mqtt
     /// </summary>
     public static class MqttSslUtility
     {
-#if (!MF_FRAMEWORK_VERSION_V4_2 && !MF_FRAMEWORK_VERSION_V4_3 && !COMPACT_FRAMEWORK && !NANOFRAMEWORK_1_0)
+#if (!MF_FRAMEWORK_VERSION_V4_2 && !MF_FRAMEWORK_VERSION_V4_3 && !COMPACT_FRAMEWORK)
         public static SslProtocols ToSslPlatformEnum(MqttSslProtocols mqttSslProtocol)
         {
             switch (mqttSslProtocol)
             {
                 case MqttSslProtocols.None:
                     return SslProtocols.None;
+#if !(NANOFRAMEWORK_1_0)
                 case MqttSslProtocols.SSLv3:
                     return SslProtocols.Ssl3;
+#endif
                 case MqttSslProtocols.TLSv1_0:
                     return SslProtocols.Tls;
                 case MqttSslProtocols.TLSv1_1:
@@ -486,7 +488,7 @@ namespace uPLibrary.Networking.M2Mqtt
                     throw new ArgumentException("SSL/TLS protocol version not supported");
             }
         }
-#elif (MF_FRAMEWORK_VERSION_V4_2 || MF_FRAMEWORK_VERSION_V4_3 || NANOFRAMEWORK_1_0)
+#elif (MF_FRAMEWORK_VERSION_V4_2 || MF_FRAMEWORK_VERSION_V4_3)
         public static SslProtocols ToSslPlatformEnum(MqttSslProtocols mqttSslProtocol)
         {
             switch (mqttSslProtocol)
@@ -495,20 +497,12 @@ namespace uPLibrary.Networking.M2Mqtt
                     return SslProtocols.None;
                 case MqttSslProtocols.TLSv1_0:
                     return SslProtocols.Tls;
-#if (NANOFRAMEWORK_1_0)
-                case MqttSslProtocols.TLSv1_1:
-                    return SslProtocols.Tls11;
-                case MqttSslProtocols.TLSv1_2:
-                    return SslProtocols.Tls12;
-                case MqttSslProtocols.SSLv3:
-#else
                 case MqttSslProtocols.TLSv1_1:
                 case MqttSslProtocols.TLSv1_2:
-#endif
                 default:
                     throw new ArgumentException("SSL/TLS protocol version not supported");
             }
         }
 #endif
-    }
-}
+            }
+        }
